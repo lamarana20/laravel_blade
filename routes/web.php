b05c155061ccb0411b0f26d140afa821862b1478
lamarana20 <162499633+lamarana20@users.noreply.github.com>
2025-03-24 16:37:03 -0400

    Déploiement: mise à jour du projet

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', [PostController::class, 'index']);
 Route::middleware(['guest'])->group(function () {
     //Register
     Route::view('/register', 'auth.register')->name('register');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
     //Login
     Route::view('/login', 'auth.login')->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
